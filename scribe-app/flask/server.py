ab1b627d8d372356ede1fcf64bfed2a4a261e61f
jalenpaige <117240799+jalenpaige@users.noreply.github.com>
2025-02-15 23:45:25 -0800

    add vector store search

--- scribe-app/flask/server.py
+++ scribe-app/flask/server.py
@@ -126,7 +126,7 @@ class ChatHandler(Resource):
     
     @app.after_request
     def add_cors_headers(response):
-        response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
+        response.headers["Access-Control-Allow-Origin"] = "http://localhost:5174"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
         response.headers["Access-Control-Allow-Credentials"] = "true"
